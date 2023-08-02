e33b2939fa891ca595be4b815f64d43799344d6d
Saad985 <shafayamjad49@gmail.com>
2023-08-03 01:47:22 +0500

    Calculations

--- tut.py
+++ tut.py
@@ -1,3 +1,12 @@
-print("hello world")
-print ("\nMy name is Saad")
-print("\n\tI live in Pakistan")
+# print("hello world")
+# print ("\nMy name is Saad")
+# print("\n\tI live in Pakistan")
+
+
+#Simple Calculations
+val1=34
+val2=74
+print(val1+val2)
+print(val2-val1)
+print(val1*val2)
+print(val2/val1)
